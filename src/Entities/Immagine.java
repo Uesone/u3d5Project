885effb0007dffe805b053e91b81317b1ac708bb
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-07-26 12:25:04 +0200

    Update Immagine.java

--- src/Entities/Immagine.java
+++ src/Entities/Immagine.java
@@ -1,4 +1,37 @@
 package Entities;
+import Constructs.ElementoMultimediale;
 
-public class Immagine {
+/**
+ * Rappresenta un'immagine con un titolo e una luminosità regolabile.
+ */
+public class Immagine extends ElementoMultimediale {
+    private int luminosita;
+
+    public Immagine(String titolo, int luminosita) {
+        super(titolo);
+        this.luminosita = luminosita;
+    }
+
+    /**
+     * Aumenta la luminosità
+     */
+    public void aumentaLuminosita() {
+        luminosita++;
+    }
+
+    /**
+     * Diminuisce la luminosità
+     */
+    public void diminuisciLuminosita() {
+        if (luminosita > 0) {
+            luminosita--;
+        }
+    }
+
+    /**
+     * Mostra l'immagine con il titolo e una serie di asterischi rappresentanti il livello di luminosità.
+     */
+    public void show() {
+        System.out.println(titolo + " " + "*".repeat(luminosita));
+    }
 }
